360ad80e858108de05a26e82d7fe6839a3721ec4
Jeanne <schiller.jeanne@gmail.com>
2015-09-03 21:46:09 -0400

    header/footer/about started

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php  wp_title('|', true, 'right'); ?></title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
   <?php // Load our CSS ?>
@@ -17,7 +19,7 @@
 <body <?php body_class(); ?>>
 
 <header>
-  <div class="container">
+  <div class="container clearfix">
     <h1>
       <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
         <?php bloginfo( 'name' ); ?>
--- page-full.php
+++ page-full.php
@@ -6,15 +6,38 @@
 
 get_header();  ?>
 
+
 <div class="main">
   <div class="container">
 
     <?php // Start the loop ?>
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-      <h2><?php the_title(); ?></h2>
-      <?php the_content(); ?>
-
+	  <div class="large-logo_container">
+	  	<div class="large-logo">
+	  		<?php the_post_thumbnail('medium'); ?>
+	  	</div>	
+	  </div> <!-- /. large-logo_container-->
+      <div class="block-text skin-red">
+      	<?php the_field("about_block"); ?>
+      </div>
+      <div class="main-content">
+      	<div class="main-content_wrapper">
+      		<?php the_title(); ?>
+	      	<div>
+	      		<?php the_field('step_1'); ?>
+	      		<?php the_field('step_1_img'); ?>
+	      	</div>	
+	      	<div>
+	      		<?php the_field('setp_2'); ?>
+	      		<?php the_field('setp_2_img'); ?>
+	      	</div>	
+	      	<div>
+	      		<?php the_field('setp_3'); ?>
+	      		<?php the_field('setp_3_img'); ?>
+	      	</div>		
+      	</div>
+      </div>
+      
     <?php endwhile; // end the loop?>
   </div> <!-- /.container -->
 </div> <!-- /.main -->
